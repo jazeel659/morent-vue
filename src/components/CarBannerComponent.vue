e6a5f1ff1334d05d2daba065c6266f32e61b4faa
jazeel659 <jaseel0659@gmail.com>
2022-08-12 01:10:48 +0530

    car banner component created

---
+++ src/components/CarBannerComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ad-banner">
+    <div class="ad-text-wrapper">
+      <h1>The Best Platform for Car Rental</h1>
+      <p>
+        Ease of doing a car rental safely and reliably. Of course at a low
+        price.
+      </p>
+      <button>Rental Car</button>
+    </div>
+    <img src="@/assets/cars/koenigsegg-banner.png" alt="" />
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+.ad-banner {
+  background-color: #5caffc;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+}
+.ad-text-wrapper {
+  font-family: "Plus Jakarta Sans";
+  color: white;
+}
+.ad-text-wrapper h1 {
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 120%;
+}
+.ad-text-wrapper p {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 160%;
+  margin-top: 16px;
+}
+.ad-banner .ad-text-wrapper button {
+  background: #3563e9;
+  border-radius: 4px;
+  padding: 10px 20px;
+  margin-top: 20px;
+  border: none;
+  outline: none;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+}
+.ad-banner img {
+  align-self: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
-
-
 <template>
   <main>
-    <h1>home</h1>
+    <car-banner-component />
   </main>
 </template>
+<script>
+import CarBannerComponent from "../components/CarBannerComponent.vue";
+export default {
+  components: {
+    CarBannerComponent,
+  },
+};
+</script>
